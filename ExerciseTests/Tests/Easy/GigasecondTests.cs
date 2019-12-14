3f2110a7fa908a839152d296db0a25298d2a9e02
Nathan <natemi1010@gmail.com>
2019-12-14 06:17:00 +0000

    Fixed project file naming and added gigasecond exercise

---
+++ ExerciseTests/Tests/Easy/GigasecondTests.cs
@@ -0,0 +1,25 @@
+﻿using GigasecondExercise;
+using NUnit.Framework;
+using System;
+
+namespace ExerciseTests.Tests.Easy
+{
+    public class GigasecondTests
+    {
+        [Test]
+        public void InOneGigasecond_SpecifyOnlyTime_TimeIsCorrect()
+        {
+            Assert.AreEqual(
+                new DateTime(2043, 1, 1, 1, 46, 40),
+                Gigasecond.InOneGigasecond(new DateTime(2011, 4, 25)));
+        }
+
+        [Test]
+        public void InOneGigasecond_SpecifyFullTime_TimeIsCorrect()
+        {
+            Assert.AreEqual(
+                new DateTime(2046, 10, 2, 23, 46, 40),
+                Gigasecond.InOneGigasecond(new DateTime(2015, 1, 24, 22, 0, 0)));
+        }
+    }
+}
