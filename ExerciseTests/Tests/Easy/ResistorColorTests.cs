6c7fe7aae869ab21beb9e8b5a40867b846e9e8a3
Nathan <natemi1010@gmail.com>
2019-12-14 06:34:08 +0000

    Added resistor color exercise

---
+++ ExerciseTests/Tests/Easy/ResistorColorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ResistorColorExercise;
+
+namespace ExerciseTests.Tests.Easy
+{
+    public class ResistorColorTests
+    {
+        [Test]
+        [TestCase("Black", 0)]
+        [TestCase("White", 9)]
+        [TestCase("Orange", 3)]
+        public void CodeForColor_GivenColor_ReturnsCodeForColor(string color, int code)
+        {
+            Assert.AreEqual(code, ResistorColor.CodeForColor(color));
+        }
+    }
+}
---
+++ ResistorColorExercise/ResistorColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ResistorColorExercise
+{
+    public static class ResistorColor
+    {
+        private static readonly string[] Colors = new[]
+        {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+        };
+
+        public static int CodeForColor(string color)
+        {
+            return Array.FindIndex(Colors, t => t.Equals(color, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
