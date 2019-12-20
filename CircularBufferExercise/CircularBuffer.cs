438db836b6cff9060ce61d766cdbb9be0e2bf534
Nathan <natemi1010@gmail.com>
2019-12-20 11:59:21 +0000

    Added circular buffer exercise

---
+++ CircularBufferExercise/CircularBuffer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises.Medium.CircularBufferExercise
+{
+    public class CircularBuffer<T>
+    {
+        private readonly int capacity;
+        private readonly List<T> buffer;
+
+        public CircularBuffer(int capacity)
+        {
+            this.capacity = capacity;
+
+            buffer = new List<T>(capacity);
+        }
+
+        public void Write(T value)
+        {
+            if (buffer.Count == capacity)
+            {
+                throw new InvalidOperationException();
+            }
+
+            buffer.Add(value);
+        }
+
+        public void Overwrite(T value)
+        {
+            if (buffer.Count < capacity)
+            {
+                Write(value);
+                return;
+            }
+
+            buffer.RemoveAt(0);
+            buffer.Add(value);
+        }
+
+        public T Read()
+        {
+            if (buffer.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var value = buffer[0];
+
+            buffer.RemoveAt(0);
+
+            return value;
+        }
+
+        public void Clear() => buffer.Clear();
+    }
+}
---
+++ ExerciseTests/Medium/CircularBufferTests.cs
@@ -0,0 +1,166 @@
+﻿using Exercises.Medium.CircularBufferExercise;
+using NUnit.Framework;
+using System;
+
+namespace Exercises.ExerciseTests.Medium
+{
+    public class CircularBufferTests
+    {
+        [Test]
+        public void Read_EmptyBuffer_ThrowsException()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            Assert.Throws<InvalidOperationException>(() => buffer.Read());
+        }
+
+        [Test]
+        public void Read_ItemJustWritten_IsRead()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Write(1);
+
+            Assert.AreEqual(1, buffer.Read());
+        }
+
+        [Test]
+        public void Read_ItemHasBeenRead_ThowsException()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Write(1);
+
+            Assert.AreEqual(1, buffer.Read());
+            Assert.Throws<InvalidOperationException>(() => buffer.Read());
+        }
+
+        [Test]
+        public void Read_ItemsAreReadInOrderOfBeingAdded()
+        {
+            var buffer = new CircularBuffer<int>(2);
+
+            buffer.Write(1);
+            buffer.Write(2);
+
+            Assert.AreEqual(1, buffer.Read());
+            Assert.AreEqual(2, buffer.Read());
+        }
+
+        [Test]
+        public void Write_FullBuffer_ThrowsException()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Write(1);
+
+            Assert.Throws<InvalidOperationException>(() => buffer.Write(2));
+        }
+
+        [Test]
+        public void Read_FullBuffer_CreatesRoomToBeWrittenTo()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Write(1);
+            Assert.AreEqual(1, buffer.Read());
+
+            buffer.Write(2);
+            Assert.AreEqual(2, buffer.Read());
+        }
+
+        [Test]
+        public void Read_OverMultipleWrites_ReadPositionIsMaintained()
+        {
+            var buffer = new CircularBuffer<int>(3);
+
+            buffer.Write(1);
+            buffer.Write(2);
+
+            Assert.AreEqual(1, buffer.Read());
+
+            buffer.Write(3);
+
+            Assert.AreEqual(2, buffer.Read());
+            Assert.AreEqual(3, buffer.Read());
+        }
+
+        [Test]
+        public void Clear_ItemsClearedCanNotBeRead()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Write(1);
+            buffer.Clear();
+
+            Assert.Throws<InvalidOperationException>(() => buffer.Read());
+        }
+
+        [Test]
+        public void Clear_FullBuffer_CanNowBeWrittenTo()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Write(1);
+            buffer.Clear();
+            buffer.Write(2);
+
+            Assert.AreEqual(2, buffer.Read());
+        }
+
+        [Test]
+        public void Clear_EmptyBuffer_DoesNothing()
+        {
+            var buffer = new CircularBuffer<int>(1);
+
+            buffer.Clear();
+            buffer.Write(1);
+
+            Assert.AreEqual(1, buffer.Read());
+        }
+
+        [Test]
+        public void Overwrite_NonFullBuffer_ActsLikeWrite()
+        {
+            var buffer = new CircularBuffer<int>(2);
+
+            buffer.Write(1);
+            buffer.Overwrite(2);
+
+            Assert.AreEqual(1, buffer.Read());
+            Assert.AreEqual(2, buffer.Read());
+        }
+
+        [Test]
+        public void Overwrite_FullBuffer_ReplacesOldestItem()
+        {
+            var buffer = new CircularBuffer<int>(2);
+
+            buffer.Write(1);
+            buffer.Write(2);
+            buffer.Overwrite(3);
+
+            Assert.AreEqual(2, buffer.Read());
+            Assert.AreEqual(3, buffer.Read());
+        }
+
+        [Test]
+        public void Overwrite_AfterAReadOnAFullBufferAndAWrite_ReplacesOldestItem()
+        {
+            var buffer = new CircularBuffer<int>(3);
+
+            buffer.Write(1);
+            buffer.Write(2);
+            buffer.Write(3);
+
+            Assert.AreEqual(1, buffer.Read());
+
+            buffer.Write(4);
+            buffer.Overwrite(5);
+
+            Assert.AreEqual(3, buffer.Read());
+            Assert.AreEqual(4, buffer.Read());
+            Assert.AreEqual(5, buffer.Read());
+        }
+    }
+}
